685f17f2029e8163115d3282fb4b1536470b1530
Shamil331 <55633377+Shamil331@users.noreply.github.com>
2022-03-29 14:06:51 +0300

    Add files via upload

---
+++ BARSGroupEvents/BARSGroupEvents/EventOnChar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BARSGroupEvents
+{
+    public class EventOnChar
+    {
+        public event EventHandler<char> ? OnKeyPressed;
+        public void Run()
+        {
+            char PressedChar = Console.ReadKey().KeyChar;
+            while (PressedChar!='c')
+            {
+                OnKeyPressed?.Invoke(this, PressedChar);
+                PressedChar=Console.ReadKey().KeyChar;
+            }
+            Console.WriteLine("\nБыла нажата 'c'");
+        }
+    }
+}
---
+++ BARSGroupEvents/BARSGroupEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BARSGroupEvents
+{
+    public class Program
+    {
+        static void Main(string []args)
+        {
+            var PressedKey = new EventOnChar();
+            PressedKey.OnKeyPressed += PressedKey_OnKeyPressed;
+            PressedKey.Run();
+        }
+        private static void PressedKey_OnKeyPressed(object sender, char e)
+        {
+            Console.WriteLine($"-{e}");
+        }
+
+    }
+}
